ca317b5d6926d329851bb50e39917fb91d571b5c
vishaldeep9 <09vishaldeep@gmail.com>
2024-02-14 11:02:39 +0530

    some updation

--- src/Component/Util/Header.jsx
+++ src/Component/Util/Header.jsx
@@ -5,24 +5,26 @@ import logo from "../../logo.png"
 const Header = () => {
   return (
     <header className="head bg-blue-900 flex justify-evenly h-16 w-full">
+      {/* strarting to search bar */}
     <div className="flex justify-around h-full items-center ">
       {/* LOGO */}
       <Link to={"/home"} className="pr-20 h-8 ">
         <img src={logo} alt="" className=" w-35 h-full" />
       </Link>
       {/* SEARCH BAR */}
-      <div className="bg-blue-50 h-9 pr-18 rounded-s-lg">
+      <div className="bg-blue-50 h-9 pr-4 ml-28 rounded-s-lg ">
         <input
           type="text"
           placeholder="Search for products, categories etc..."
-          className=" min-w-96 pl-8 pr-8 h-full rounded-sm bg-blue-50"
-        ></input>
-        <i class="fa-solid fa-magnifying-glass"></i>
+          className=" min-w-96 pl-8 pr-4 h-full rounded-sm bg-blue-50"
+        >  </input>
+        <i class="fa-solid fa-magnifying-glass " ></i>
       </div>
     </div>
+    {/* after serach bar */}
     <div className="flex justify-evenly items-center h-full w-4/12 ">
       <Link to={"/login"}>
-        <button className="h-10 w-28 bg-white text-blue-700 font-bold">
+        <button className="h-8 w-28 bg-white text-blue-700 font-bold">
           Login
         </button>
       </Link>
